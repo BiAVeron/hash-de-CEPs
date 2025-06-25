1691cbfe643d12114f06062cf4009e9a0c1ac208
Beatriz Veron <biaveron359@gmail.com>
2025-06-24 21:14:57 -0300

    feat: adicionar suporte a hash duplo e parametrização do tipo de hashing

---
+++ hash.c
@@ -0,0 +1,235 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#define SEED    0x12345678
+
+typedef struct {
+     uintptr_t * table;
+     int size;
+     int max;
+     uintptr_t deleted;
+     char * (*get_key)(void *);
+     tipoHash tipo; //vamos definir se vai ser simples ou duplo
+}thash;
+
+// criei a segunda função da hash dupla
+uint32_t hashf2(const char *str){
+    uint32_t h = 0;
+    for (; *str; ++str){
+        h = (h * 33) ^ (unsigned char)(*str);
+    }
+    return (h % 97) + 1;
+}
+
+uint32_t hashf(const char* str, uint32_t h){
+    /* One-byte-at-a-time Murmur hash 
+    Source: https://github.com/aappleby/smhasher/blob/master/src/Hashes.cpp */
+    for (; *str; ++str) {
+        h ^= *str;
+        h *= 0x5bd1e995;
+        h ^= h >> 15;
+    }
+    return h;
+}
+
+//alterei essa função tbm pra receber os dois tipos, assim como nas funções de inserir e remover
+int hash_insere(thash * h, void * bucket){
+    uint32_t hash = hashf(h->get_key(bucket));
+    uint32_t hash2 = (h->tipo == HASH_DUPLO) ? hashf2(key) : 1;
+    int i = 0;
+    int pos;
+
+    if (h->max == (h->size+1)){
+        free(bucket);
+        return EXIT_FAILURE; /* hash full*/
+    }
+    do{
+        pos = (hash + i * hash2) % h->max;
+        if (h->table[pos] == 0 || h->table[pos] == h->deleted){
+            h->table[pos] = (uintptr_t) bucket;
+            h->size++;
+            return EXIT_SUCCESS;
+        }
+        i++;
+    } while(i<h->max);
+
+    free(bucket);
+    return EXIT_SUCCESS;
+}
+
+
+
+int hash_constroi(thash * h,int nbuckets, char * (*get_key)(void *) ){
+    h->table =calloc(sizeof(void *),nbuckets+1);
+    if (h->table == NULL){
+        return EXIT_FAILURE;
+    }
+    h->max = nbuckets+1;
+    h->size = 0;
+    h->deleted = (uintptr_t)&(h->size);
+    h->get_key = get_key;
+    h->tipo = tipo; //adicionamos pra aceitar o tipo da hash
+    return EXIT_SUCCESS;
+
+}
+
+
+void * hash_busca(thash h, const char * key){
+    uint32_t hash = hashf(key,SEED);
+    uint32_t hash2 = (h.tipo == HASH_DUPLO) ? hashf2(key) : 1;
+    int i = 0;
+    int pos;
+
+    do{
+        pos = (hash + i * hash2)%h.max;
+        
+        if(h.table[pos] = 0)
+            return NULL;
+        if (strcmp(h.get_key((void *)h.table[pos]),key) == 0)
+            return (void *)h.table[pos];
+        i++;
+    }
+    while(i<h.max);
+        
+    return NULL;
+}
+
+int hash_remove(thash * h, const char * key){
+    uint32_t hash = hashf(key,SEED);
+    uint32_t hash2 = (h.tipo == HASH_DUPLO) ? hashf2(key) : 1;
+    int i = 0;
+    int pos;
+
+    do{
+        pos = (hash + i * hash2)%h.max;
+
+        if(h.table[pos] = 0)
+            return NULL;
+        if (strcmp(h.get_key((void *)h.table[pos]),key) == 0){
+            free((void *)h->table[pos]);
+            h->table[pos] = h->deleted;
+            h->size--;
+            return EXIT_SUCCESS;
+        }
+        i++;
+    }
+    while(i<h.max);
+        
+    return EXIT_FAILURE;
+}
+
+void hash_apaga(thash *h){
+    int pos;
+    for(pos =0;pos< h->max;pos++){
+        if (h->table[pos] != 0){
+            if (h->table[pos]!=h->deleted){
+                free((void *)h->table[pos]);
+            }
+        }
+    }
+    free(h->table);
+}
+
+
+
+typedef struct{
+    char nome[30];
+    char  cpf[11];
+}taluno;
+
+char * get_key(void * reg){
+    return (*((taluno *)reg)).nome;
+}
+
+
+void * aloca_aluno(char * nome, char * cpf){
+    taluno * aluno = malloc(sizeof(taluno));
+    strcpy(aluno->nome,nome);
+    strcpy(aluno->cpf,cpf);
+    return aluno;
+}
+
+
+
+void test_hash(){
+    thash h;
+    int nbuckets = 10;
+    hash_constroi(&h,nbuckets,get_key);
+    assert(hash_insere(&h,aloca_aluno("edson","0123456789"))==EXIT_SUCCESS);
+    assert(hash_insere(&h,aloca_aluno("edson","0123456789"))==EXIT_SUCCESS);
+    assert(hash_insere(&h,aloca_aluno("edson","0123456789"))==EXIT_SUCCESS);
+    assert(hash_insere(&h,aloca_aluno("edson","0123456789"))==EXIT_SUCCESS);
+    assert(hash_insere(&h,aloca_aluno("edson","0123456789"))==EXIT_SUCCESS);
+    assert(hash_insere(&h,aloca_aluno("edson","0123456789"))==EXIT_SUCCESS);
+    assert(hash_insere(&h,aloca_aluno("edson","0123456789"))==EXIT_SUCCESS);
+    assert(hash_insere(&h,aloca_aluno("edson","0123456789"))==EXIT_SUCCESS);
+    assert(hash_insere(&h,aloca_aluno("edson","0123456789"))==EXIT_SUCCESS);
+    assert(hash_insere(&h,aloca_aluno("edson","0123456789"))==EXIT_SUCCESS);
+    assert(hash_insere(&h,aloca_aluno("edson","0123456789"))==EXIT_FAILURE);
+    hash_apaga(&h);
+}
+void test_search(){
+    thash h;
+    int nbuckets = 10;
+    taluno * aluno;
+    hash_constroi(&h,nbuckets,get_key);
+    assert(hash_insere(&h,aloca_aluno("edson","0123456789"))==EXIT_SUCCESS);
+    assert(hash_insere(&h,aloca_aluno("takashi","1123456789"))==EXIT_SUCCESS);
+    assert(hash_insere(&h,aloca_aluno("matsubara","2123456789"))==EXIT_SUCCESS);
+
+    aluno = hash_busca(h,"edson");
+    assert(aluno->cpf[0]=='0');
+    aluno = hash_busca(h,"takashi");
+    assert(aluno->cpf[0]=='1');
+    aluno = hash_busca(h,"matsubara");
+    assert(aluno->cpf[0]=='2');
+    aluno = hash_busca(h,"patricia");
+    assert(aluno == NULL);
+
+    hash_apaga(&h);
+}
+
+void test_remove(){
+    thash h;
+    int nbuckets = 10;
+    taluno * aluno;
+    hash_constroi(&h,nbuckets,get_key);
+    assert(hash_insere(&h,aloca_aluno("edson","0123456789"))==EXIT_SUCCESS);
+    assert(hash_insere(&h,aloca_aluno("takashi","1123456789"))==EXIT_SUCCESS);
+    assert(hash_insere(&h,aloca_aluno("matsubara","2123456789"))==EXIT_SUCCESS);
+
+    aluno = hash_busca(h,"edson");
+    assert(aluno->cpf[0]=='0');
+    aluno = hash_busca(h,"takashi");
+    assert(aluno->cpf[0]=='1');
+    aluno = hash_busca(h,"matsubara");
+    assert(aluno->cpf[0]=='2');
+    aluno = hash_busca(h,"patricia");
+    assert(aluno == NULL);
+
+    assert(h.size == 3);
+    assert(hash_remove(&h,"edson")==EXIT_SUCCESS);
+    aluno = hash_busca(h,"edson");
+    assert(aluno == NULL);
+    assert(h.size == 2);
+
+    assert(hash_remove(&h,"edson")==EXIT_FAILURE);
+
+    aluno = hash_busca(h,"matsubara");
+    assert(aluno->cpf[0]=='2');
+
+
+    hash_apaga(&h);
+
+}
+
+
+
+int main(int argc, char* argv[]){
+    test_hash();
+    test_search();
+    test_remove();
+    return 0;
+}
